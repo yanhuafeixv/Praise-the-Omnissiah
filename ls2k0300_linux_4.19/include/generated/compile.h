cca7366a99e79e4b5b464ec243007053db2b84dd
yanhuafeixv <2494007506@qq.com>
2026-03-18 00:11:36 +0800

    test_last

--- ls2k0300_linux_4.19/include/generated/compile.h
+++ ls2k0300_linux_4.19/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 230 */
+/* This file is auto generated, version 231 */
 /* SMP */
 #define UTS_MACHINE "loongarch64"
-#define UTS_VERSION "#230 SMP Tue Mar 17 23:15:55 CST 2026"
+#define UTS_VERSION "#231 SMP Wed Mar 18 00:09:09 CST 2026"
 #define LINUX_COMPILE_BY "lhx"
 #define LINUX_COMPILE_HOST "lhx-VMware-Virtual-Platform"
 #define LINUX_COMPILER "gcc version 8.3.0 (LoongArch GNU toolchain rc1.6 (20241115))"
