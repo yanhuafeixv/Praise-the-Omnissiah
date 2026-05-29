371704c2657e74f5d9f0fff607a7b2beb542c5f0
yingdemu <email@example.com2458319912@qq.com>
2026-05-29 21:15:17 +0800

    蓝牙输入没有问题，输出不做了

--- Seekfree_LS2K0300_Opensource_Library/project/user/main.cpp
+++ Seekfree_LS2K0300_Opensource_Library/project/user/main.cpp
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
 #include "serial.h"
+#include "string.h"
+#include <stdlib.h>
+
 
 int main(void)
 {
@@ -14,17 +17,24 @@ int main(void)
     Serial_StartRx();
 
     /* 发送一条 AT 测试指令 */
-    Serial_SendString("AT\r\n");
-    sleep(1);
+    // Serial_SendString("AT\r\n");
+    // sleep(1);
+
+
+    int cnt=0;
 
     /* 主循环：等价于 STM32 中 while(1) 的使用方式 */
     while (1) {
         if (Serial_RxFlag) {
             /* 收到一帧 [xxx] 格式的数据 */
             printf("Received packet: %s\n", Serial_RxPacket);
+            printf("%d\n",strcmp(Serial_RxPacket, "up"));
+            if (strcmp(Serial_RxPacket, "up") == 0)
+            {
 
-            /* 回显 */
-            Serial_Printf("[%s]\r\n", Serial_RxPacket);
+                cnt++;
+                printf("%d\n",cnt);
+            }
 
             /* 清除标志，准备接收下一帧 */
             Serial_RxFlag = 0;
@@ -32,10 +42,11 @@ int main(void)
 
         /* 可以在这里添加其他业务逻辑 */
         usleep(10000);  /* 10ms，降低 CPU 占用 */
-        Serial_Printf("123\r\n");
 
     }
 
     Serial_Close();
     return 0;
 }
+
+
--- ls2k0300_linux_4.19/include/generated/compile.h
+++ ls2k0300_linux_4.19/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 268 */
+/* This file is auto generated, version 270 */
 /* SMP */
 #define UTS_MACHINE "loongarch64"
-#define UTS_VERSION "#268 SMP Fri May 29 18:57:39 CST 2026"
+#define UTS_VERSION "#270 SMP Fri May 29 20:34:04 CST 2026"
 #define LINUX_COMPILE_BY "yingmu"
 #define LINUX_COMPILE_HOST "yingmu-VMware-Virtual-Platform"
 #define LINUX_COMPILER "gcc version 8.3.0 (LoongArch GNU toolchain rc1.6 (20241115))"
